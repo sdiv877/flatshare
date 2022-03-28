f39bab7f2b541fb259d56e25916ff2a872fd9663
sdiv877 <57572181+sdiv877@users.noreply.github.com>
2022-03-28 20:18:57 +1300

    Add backpath prop

--- packages/frontend/src/components/common/layout/Page.tsx
+++ packages/frontend/src/components/common/layout/Page.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 import Footer from './Footer';
 import Navigation from './Navigation';
 
-interface PageProps {}
+interface PageProps {
+  backpath?: string;
+}
 
 /**
  * Generic page used by pages in the app (i.e. not public pages)
  */
-const Page: React.FC<PageProps> = ({ children }) => {
+const Page: React.FC<PageProps> = ({ children, backpath }) => {
   return (
     <div className="flex flex-col w-full min-h-screen bg-gray-50">
-      <Navigation />
+      <Navigation backpath={backpath} />
 
       <div className="flex-grow self-center px-10 max-w-5xl w-full">
         {children}
--- packages/frontend/src/components/common/util/BackButton.tsx
+++ packages/frontend/src/components/common/util/BackButton.tsx
@@ -3,19 +3,27 @@ import { Button } from '@nextui-org/react';
 import { useNavigate } from 'react-router-dom';
 import { ChevronLeftIcon } from '@heroicons/react/outline';
 
-interface BackButtonProps {}
+interface BackButtonProps {
+  backpath?: string;
+}
 
-const BackButton: React.FC<BackButtonProps> = () => {
+const BackButton: React.FC<BackButtonProps> = ({ backpath }) => {
   const navigate = useNavigate();
 
   return (
-    <Button
-      onClick={() => navigate(-1)}
-      className="flex justify-center items-center py-3 h-fit hover:bg-gray-700 hover:bg-opacity-5 transition-all"
-      auto
-      light
-      icon={<ChevronLeftIcon className="w-10" />}
-    />
+    <div>
+      {backpath && (
+        <div className="-ml-11 mr-2">
+          <Button
+            onClick={() => navigate(backpath)}
+            className="flex justify-center items-center py-3 h-fit hover:bg-gray-700 hover:bg-opacity-5 transition-all"
+            auto
+            light
+            icon={<ChevronLeftIcon className="w-10" />}
+          />
+        </div>
+      )}
+    </div>
   );
 };
 
